230e1073ad5521c7c8dbc703c27e36432faacce6
Mariam Qasim <mqfanhero@Mariams-MacBook-Pro.local>
2017-05-16 15:39:09 -0400

    Finishing weather app

--- grider/weather_app/weatherapp/src/containers/weather_list.js
+++ grider/weather_app/weatherapp/src/containers/weather_list.js
@@ -1,26 +1,43 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
-    console.log(cityData)
+    const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const humidity = cityData.list.map(weather => weather.main.humidity);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    // const lon = cityData.city.coord.lon;
+    // const lat = cityData.city.coord.lat;
+    const { lon, lat } = cityData.city.coord;
+
     return(
-      <tr>
-        <td>{cityData.city.name}</td>
+      <tr key={name}>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
+        <td>
+          <Chart data={temps} color="pink" units="K"/>
+        </td>
+        <td>
+          <Chart data={pressures} color="orange" units="hPa"/>
+        </td>
+        <td>
+          <Chart data={humidity} color="yellow" units="%"/>
+        </td>
       </tr>
     );
   }
 
   render(){
-    console.log("anything here?" , this.props);
     return (
       <table className="table table-hover">
         <thead>
           <tr>
             <th>City</th>
-            <th>Temp</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temp (K)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
